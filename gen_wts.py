10fa1a196b5bbfaa5259b1fb900cc87075eb2da1
Yijun Jiang <yijun1121@gmail.com>
2022-07-26 15:05:45 -0400

    fix input channel issue

--- gen_wts.py
+++ gen_wts.py
@@ -17,6 +17,7 @@ def parse_args():
     parser.add_argument('-w', '--weights', required=True, help='Input weights (.pt) file path')
     parser.add_argument('-c', '--num_classes', required=True, help='the number of classes in the trained model')
     parser.add_argument('-o', '--out_file', required=True, help='the output weight file path')
+    parser.add_argument('--inc', default=3, type=int, help='the number of image channels')
     parser.add_argument('--model', default='efficientnet-b0', help='the name of the model, such as: efficientnet-b0')
     args = vars(parser.parse_args())
     if not os.path.isfile(args['weights']):
@@ -27,7 +28,8 @@ args = parse_args()
 
 num_classes = int(args['num_classes'])
 out_file = args['out_file']
-model = EfficientNet.from_pretrained(args['model'], num_classes=num_classes).to(torch.double)
+inc = args['inc']
+model = EfficientNet.from_pretrained(args['model'], num_classes=num_classes, in_channels=inc).to(torch.double)
 model.load_state_dict(torch.load(args['weights']))
 model.eval()
 
